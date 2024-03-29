7c12682a16f0eb17e69c66943f31b4d3afc6883f
nasir-abak <muhammadnasir.abak@gmail.com>
2024-03-30 01:27:53 +0500

    push my calculator to  githup

---
+++ index.js
@@ -0,0 +1,25 @@
+#! /user/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "firstnumber" },
+    { message: "Enter second number", type: "number", name: "secondnumber" },
+    {
+        message: "Select one of the operator", type: "list", name: "operator", choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+// conditional statement
+if (answer.operator === "Addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
+else {
+    console.log("Please Select a Valid Operator");
+}
